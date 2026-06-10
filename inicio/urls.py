c968fb494e9840b6d0302c8aa1d306443582f666
William <william@sanis.com>
2026-06-10 10:40:28 -0500

    william cambios 4 - limpiar urls inicio

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from django.urls import path   , include                                     
+from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('',views.inicio, name='inicio'),
-    path('doctores/',views.doctores, name='doctores'),
-    path('departamentos/',views.departamentos, name='departamentos'),
-    path('citas/',views.citas, name='citas'),
-    path('login/',views.login_view, name='login'),
+    path('', views.inicio, name='inicio'),
+    path('doctores/', views.doctores, name='doctores'),
+    path('departamentos/', views.departamentos, name='departamentos'),
+    path('citas/', views.citas, name='citas'),
+    path('login/', views.login_view, name='login'),
 ]
