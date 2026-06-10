0ba375cd2a4c423c6f97f207b379019d172d6f48
William <william@sanis.com>
2026-06-10 10:42:53 -0500

    william cambios 9 - rutas dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import dashboard
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('inicio/', views.dashboard, name='dashboard_inicio'),
 ]
