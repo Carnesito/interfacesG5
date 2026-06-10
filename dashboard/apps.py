a56babacb01c8899e076d36a4fc54d531170a167
William <william@sanis.com>
2026-06-10 10:42:30 -0500

    william cambios 8 - limpiar apps

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
-
 class DashboardConfig(AppConfig):
-<<<<<<< HEAD
-=======
     default_auto_field = 'django.db.models.BigAutoField'
->>>>>>> respaldo-local
     name = 'dashboard'
