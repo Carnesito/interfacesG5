ebfcaae433aa0440c301020d3cc17f0d562e2d43
William <william@sanis.com>
2026-06-10 11:13:21 -0500

    william cambios 12 - cambiar db a sqlite y actualizar requirements

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'interfacesG5',
-        'USER': 'postgres',
-        'PASSWORD': '2005',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
