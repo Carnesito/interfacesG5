019aa98b790b4ad44e6269d3f49fbe9f0994128a
William <william@sanis.com>
2026-06-10 10:39:58 -0500

    william cambios 3 - limpiar dashboard views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-from django.http import HttpResponse
-
-# Create your views here.
-=======
 from django.contrib.auth.decorators import login_required
 
-
 @login_required(login_url='login')
->>>>>>> respaldo-local
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
